0f3cd2946c059f45092a9cdf8480444756c2f420
Mel-Rocha <rochamel73@gmail.com>
2024-04-18 23:45:44 -0300

    class

--- scraping/ikesaki/extract.py
+++ scraping/ikesaki/extract.py
@@ -1,4 +1,5 @@
 import re
+
 import requests
 from bs4 import BeautifulSoup
 
@@ -6,30 +7,27 @@ from bs4 import BeautifulSoup
 class ExtractProductPrice:
     def __init__(self, url):
         self.url = url
+        self.response = self._get_response()
+        self.soup = BeautifulSoup(self.response.content, 'html.parser')
 
-    def extract_product(self):
+    def _get_response(self):
         response = requests.get(self.url)
-
         if response.status_code != 200:
             raise Exception("URL inválida ou não encontrada")
+        return response
 
-        soup = BeautifulSoup(response.content, 'html.parser')
-
-        product_element = soup.find('h1')
+    def extract_product(self):
+        product_element = self.soup.find('h1')
         product = product_element.text.strip() if product_element else None
-
         return product
 
     def extract_price(self):
-        response = requests.get(self.url)
-
-        if response.status_code != 200:
+        if self.response.status_code != 200:
             raise Exception("URL inválida ou não encontrada")
 
         price_from_regex = None
-
         pattern = r'"Value"\s*:\s*([\d.]+)'
-        matches = re.findall(pattern, response.text)
+        matches = re.findall(pattern, self.response.text)
 
         if matches:
             price_from_regex = matches[0]
