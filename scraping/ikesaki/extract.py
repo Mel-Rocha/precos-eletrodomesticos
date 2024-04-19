5ec4a176ecffc232c4971cdc9fe299bad875628b
Mel-Rocha <rochamel73@gmail.com>
2024-04-18 23:11:19 -0300

    price wrong

--- scraping/ikesaki/extract.py
+++ scraping/ikesaki/extract.py
@@ -1,12 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
+import re
 
-def scrape_product_info(url):
+
+def scrape_product_price(url):
     # Realiza o request para a URL
     response = requests.get(url)
+
     # Verifica se a requisição foi bem-sucedida
     if response.status_code != 200:
         raise Exception("URL inválida ou não encontrada")
@@ -14,14 +14,26 @@ def scrape_product_info(url):
     # Parseia o conteúdo HTML
     soup = BeautifulSoup(response.content, 'html.parser')
 
-    # Encontra o título do produto
-    product_element = soup.find('h1')
-    product = product_element.text.strip() if product_element else None
+    # Encontra todos os elementos que contêm "$"
+    elementos_com_dollar = soup.find_all(text=lambda text: text and '$' in text)
 
-    return product
+    # Para cada elemento encontrado, extrai o valor
+    for elemento in elementos_com_dollar:
+        valor = elemento.split('$')[-1].strip()
+        print("Valor encontrado (do BeautifulSoup):", valor)
 
-url = 'https://www.ikesaki.com.br/coloracao-igora-royal-7-00-louro-medio-natural-extra-60g/p'
+    # Encontra todos os elementos que contêm a chave "Value"
+    pattern = r'"Value"\s*:\s*([\d.]+)'
+    matches = re.findall(pattern, response.text)
 
-product_info_soup = scrape_product_info(url)
-print("Produto (BeautifulSoup):", product_info_soup)
+    # Se houver correspondências, extrai o valor encontrado
+    if matches:
+        valor_encontrado = matches[0]
+        print("Valor encontrado (da expressão regular):", valor_encontrado)
+    else:
+        print("Nenhum valor correspondente encontrado.")
 
+
+# Teste da função
+url = "https://www.ikesaki.com.br/coloracao-igora-royal-7-00-louro-medio-natural-extra-60g/p"
+scrape_product_price(url)
