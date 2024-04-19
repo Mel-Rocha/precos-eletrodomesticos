fdbbe3b744fe5f9ad423cb6ad2e3ae8693968661
Mel-Rocha <rochamel73@gmail.com>
2024-04-19 18:23:26 -0300

    working ikesaki

--- automation/search.py
+++ automation/search.py
@@ -1,9 +1,9 @@
 import time
-
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from bs4 import BeautifulSoup
 
 
 def search_product_on_site(site_url, product):
@@ -12,9 +12,38 @@ def search_product_on_site(site_url, product):
 
     driver.get(site_url)
     time.sleep(2)
-    driver.find_element('xpath', '//*[@id="downshift-0-input"]').send_keys(product, Keys.ENTER)
+    search_box = driver.find_element('xpath', '//*[@id="downshift-0-input"]')
+    search_box.send_keys(product, Keys.ENTER)
     time.sleep(5)
 
+    # Pegando o HTML da página após a pesquisa
+    html_content = driver.page_source
+
+    # Utilizando BeautifulSoup para analisar o HTML
+    soup = BeautifulSoup(html_content, 'html.parser')
+
+    # Encontrar a primeira imagem dentro da div com ID "gallery-layout-container"
+    gallery_div = soup.find('div', {'id': 'gallery-layout-container'})
+    if gallery_div:
+        first_img = gallery_div.find('img')
+        if first_img:
+            # Encontrar o elemento da imagem com o Selenium
+            img_element = driver.find_element('xpath', '//*[@id="gallery-layout-container"]/div/section/a/article'
+                                                       '/div[1]/div[1]/div/div/img')
+
+            # Clicar na imagem usando o Selenium
+            img_element.click()
+
+            # Aguardar um curto período para redirecionamento
+            time.sleep(3)
+
+            # Verificar se a URL mudou após o clique na imagem
+            if driver.current_url != site_url:
+                print("Redirecionado para:", driver.current_url)
+            else:
+                print("Não houve redirecionamento após clicar na imagem.")
+
+    time.sleep(5)
     driver.quit()
 
 
@@ -26,5 +55,5 @@ sites = [
 ]
 
 for site in sites:
-    search_product_on_site(site["url"], product)
 
+    search_product_on_site(site["url"], product)
