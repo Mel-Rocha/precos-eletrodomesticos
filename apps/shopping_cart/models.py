08810e73d79ca81b94d59151ebbba6e4d191cdc4
Mel-Rocha <rochamel73@gmail.com>
2024-04-19 08:25:34 -0300

    models ShoppingCart

--- apps/shopping_cart/models.py
+++ apps/shopping_cart/models.py
@@ -5,7 +5,7 @@ from tortoise import fields, Model
 
 class ShoppingCart(Model):
     id = fields.UUIDField(pk=True, default=uuid.uuid4())
-    product_name = fields.CharField(max_length=255)
-    description = fields.TextField()
-    value = fields.DecimalField(max_digits=10, decimal_places=2)
+    product = fields.CharField(max_length=255)
+    price = fields.DecimalField(max_digits=10, decimal_places=2)
     store = fields.CharField(max_length=50)
+    description = fields.CharField(max_length=255, blank=True, null=True)
