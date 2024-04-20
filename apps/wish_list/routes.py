db60d7585cf06871c10b9a2629a583c484bd63a9
Mel-Rocha <rochamel73@gmail.com>
2024-04-20 11:05:50 -0300

    error handling upload

--- apps/wish_list/routes.py
+++ apps/wish_list/routes.py
@@ -5,6 +5,7 @@ from fastapi.responses import JSONResponse
 from fastapi_pagination import Page, add_pagination, paginate
 from fastapi import APIRouter, UploadFile, File
 
+from apps.wish_list.exceptions import InvalidColumnsException
 from apps.wish_list.models import WishList
 from apps.wish_list.schema import WishListSchema
 
@@ -22,11 +23,20 @@ add_pagination(router)
 @router.post("/upload")
 async def upload_wish_list(file: UploadFile = File(...)):
     try:
+        # File reading
         contents = await file.read()
         file_data = BytesIO(contents)
 
+        # File Processing
         df = pd.read_excel(file_data)
 
+        # Validation columns
+        expected_columns = {'PRODUCT_NAME', 'SITE_DOMAIN', 'EXPECTED_PURCHASE_DATE', 'DESIRE_TO_ACQUIRE',
+                            'NEED_TO_ACQUIRE'}
+        if set(df.columns) != expected_columns:
+            raise InvalidColumnsException(status_code=400, detail="Upload Failed")
+
+        # Column treatment
         df['product_name'] = df['PRODUCT_NAME'].astype(str).str.strip()
         df = df.drop('PRODUCT_NAME', axis=1)
 
@@ -42,12 +52,20 @@ async def upload_wish_list(file: UploadFile = File(...)):
         df['need_to_acquire'] = df['NEED_TO_ACQUIRE'].astype(str).str.strip()
         df = df.drop('NEED_TO_ACQUIRE', axis=1)
 
+        # Format conversion required to create bulk objects
         data_to_insert = df.to_dict(orient='records')
 
+        # Bulk creation
         await WishList.bulk_create([WishList(**row) for row in data_to_insert])
 
         return JSONResponse(content={"message": "Wish list uploaded successfully"},
                             status_code=201)
+
+    except InvalidColumnsException as e:
+        return JSONResponse(content={"message": f'{str(e)}, The spreadsheet must contain these columns PRODUCT_NAME, '
+                                                f'SITE_DOMAIN, EXPECTED_PURCHASE_DATE, DESIRE_TO_ACQUIRE, '
+                                                f'NEED_TO_ACQUIRE'},
+                            status_code=400)
 
     except Exception as e:
         return JSONResponse(content={"message": str(e)},
