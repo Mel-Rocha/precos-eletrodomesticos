316625a5b0c4fdb8ba3b068b8245091b3c6894d0
Mel-Rocha <rochamel73@gmail.com>
2024-04-19 21:30:06 -0300

    half working, still doesn't call the extraction route

--- automation/search.py
+++ automation/search.py
@@ -7,45 +7,58 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
 
-def search_product_on_site(site_url, product):
-    service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service)
+class AutomationSearchProduct:
+    def __init__(self):
+        self.service = Service(ChromeDriverManager().install())
+        self.driver = None
 
-    driver.get(site_url)
-    time.sleep(2)
-    search_box = driver.find_element('xpath', '//*[@id="downshift-0-input"]')
-    search_box.send_keys(product, Keys.ENTER)
-    time.sleep(5)
+    def start_driver(self):
+        self.driver = webdriver.Chrome(service=self.service)
 
-    html_content = driver.page_source
+    def stop_driver(self):
+        if self.driver:
+            self.driver.quit()
 
-    soup = BeautifulSoup(html_content, 'html.parser')
+    def search_product_on_site(self, site_url, product):
+        self.start_driver()
 
-    gallery_div = soup.find('div', {'id': 'gallery-layout-container'})
-    if gallery_div:
-        first_img = gallery_div.find('img')
-        if first_img:
-            img_element = driver.find_element('xpath', '//*[@id="gallery-layout-container"]/div/section/a/article'
-                                                       '/div[1]/div[1]/div/div/img')
+        self.driver.get(site_url)
+        time.sleep(2)
+        search_box = self.driver.find_element('xpath', '//*[@id="downshift-0-input"]')
+        search_box.send_keys(product, Keys.ENTER)
+        time.sleep(5)
 
-            img_element.click()
+        html_content = self.driver.page_source
 
-            time.sleep(3)
+        soup = BeautifulSoup(html_content, 'html.parser')
 
-            if driver.current_url != site_url:
-                print("Redirecionado para:", driver.current_url)
-            else:
-                print("Não houve redirecionamento após clicar na imagem.")
+        gallery_div = soup.find('div', {'id': 'gallery-layout-container'})
+        if gallery_div:
+            first_img = gallery_div.find('img')
+            if first_img:
+                img_element = self.driver.find_element('xpath', '//*[@id="gallery-layout-container"]/div/section/a/article'
+                                                           '/div[1]/div[1]/div/div/img')
 
-    time.sleep(5)
-    driver.quit()
+                img_element.click()
+
+                time.sleep(3)
+
+                if self.driver.current_url != site_url:
+                    print("Redirecionado para:", self.driver.current_url)
+                else:
+                    print("Não houve redirecionamento após clicar na imagem.")
+
+        time.sleep(5)
+        self.stop_driver()
+
+        # return self.driver.current_url
 
 
-product = "Coloração Igora Royal 8.77 Louro Claro Cobre Extra 60g"
+# sites = [
+#     {"url": "https://www.ikesaki.com.br/"},
+# ]
 
-sites = [
-    {"url": "https://www.ikesaki.com.br/"},
-]
-
-for site in sites:
-    search_product_on_site(site["url"], product)
+# product = "Coloração Igora Royal 8.77 Louro Claro Cobre Extra 60g"
+# automation = AutomationSearchProduct()
+# for site in sites:
+#     automation.search_product_on_site(site["url"], product)
