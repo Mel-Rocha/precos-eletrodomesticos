66a76c028ac7117f7acfed96720ad8968c487aaf
Mel-Rocha <rochamel73@gmail.com>
2024-04-19 22:24:55 -0300

    half working

--- apps/product/routes.py
+++ apps/product/routes.py
@@ -1,8 +1,19 @@
+import os
+
+import requests
 from fastapi import APIRouter, HTTPException
+from dotenv import load_dotenv
 
 from scraping.product.extract import ExtractProductPriceStore
+from automation.search import AutomationSearchProduct
+
+load_dotenv()
 
 router = APIRouter()
+
+AUTH_TOKEN = os.getenv('AUTH_TOKEN')
+LOCALHOST = os.getenv('LOCALHOST')
+
 
 
 @router.get("/extract/")
@@ -13,5 +24,15 @@ async def extract_product_price_store(url: str):
         price = extractor.extract_price()
         store = extractor.extract_store()
         return {"product": product, "price": price, "store": store}
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+
+@router.get("/automation/product/")
+async def automation_product(site_domain: str, product: str):
+    try:
+        automation = AutomationSearchProduct()
+        automation.search_product_on_site(site_domain, product)
+        return {"message": "Product search completed"}
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
