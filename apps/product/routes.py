79c710e29fbc1f69dbd2229e8367d3e8e1d24b11
Mel-Rocha <rochamel73@gmail.com>
2024-04-23 11:24:40 -0300

    document specifying route returns to support frontend integrations

--- apps/product/routes.py
+++ apps/product/routes.py
@@ -16,21 +16,87 @@ router = APIRouter()
 
 @router.get("/extract/")
 async def extract_product_price_store(url: str):
+    """
+    Objective:
+        This endpoint is responsible for extracting product information from a URL.
+
+
+    Parameters:
+        Mandatory:
+        url (str): The URL of the product page.
+
+    Returns:
+        If the operation is successful, the dictionary contains the keys 'product', 'price' and 'store'.
+
+        If an error occurs while extract product information, the return is a JSONResponse with the key
+        detail and the status_code.
+
+        The 'status_code' is an HTTP status code that indicates the result of the operation. It could be one of the
+        following:
+        - 200: The search and removal of product information was successful.
+        - 400: An error occurred while searching or removing product information.
+    """
+
     return await extract_info_from_url(url)
 
 
 @router.get("/automation/")
 async def automation_product(product: str, site_domain: str = 'https://www.ikesaki.com.br'):
+    """
+    Objective:
+        This endpoint is responsible for automating the process of extracting product information from a website.
+
+
+    Parameters:
+        Mandatory:
+        product (str): The name of the product to be searched.
+
+        Optional:
+        site_domain (str): The domain of the site where the product will be searched.
+
+
+    Returns:
+        If the operation is successful, the dictionary contains the keys 'product', 'price' and 'store'.
+
+        If an error occurs while searching or removing product information, the return is a JSONResponse with the key
+        detail and the status_code.
+
+        The 'status_code' is an HTTP status code that indicates the result of the operation. It could be one of the
+        following:
+        - 200: The search and removal of product information was successful.
+        - 500: An error occurred while searching or removing product information.
+
+    """
+
     try:
         automation = AutomationSearchProduct()
         redirected_url = automation.search_product_on_site(product, site_domain)
         return await extract_info_from_url(redirected_url)
     except Exception as e:
-        raise HTTPException(status_code=400, detail=str(e))
+        raise HTTPException(status_code=500, detail=str(e))
 
 
 @router.get("/create_or_update/")
 async def create_or_update():
+    """
+    Objective:
+        This endpoint is responsible for creating or updating products based on the information in the WishList table.
+
+
+    Returns:
+        JSONResponse: A JSONResponse object that contains a 'content' dictionary and a 'status_code'.
+
+        The 'content' dictionary contains the key 'message', which can have one of the following values:
+        - "Product created": A new product has been created successfully.
+        - "Product updated": An existing product has been updated successfully.
+        - An error message, an error will occur while creating or updating the product. The 'status_code'
+
+        is an HTTP status code that indicates the result of the operation. It could be one of the following:
+        - 201: A new product has been created successfully.
+        - 200: An existing product has been successfully updated.
+        - 500: An error occurred while creating or updating the product.
+    """
+
     # Returns variables
     messages = {}
     status_code = None
