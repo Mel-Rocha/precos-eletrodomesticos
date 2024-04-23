418f4818fa4f8fc5042e58d20a202726fc92ff92
Mel-Rocha <rochamel73@gmail.com>
2024-04-23 15:28:11 -0300

    collect url of all products, not working very well, initial version

--- automation/search.py
+++ automation/search.py
@@ -5,6 +5,10 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 
 class AutomationSearchProduct:
@@ -20,15 +24,6 @@ class AutomationSearchProduct:
             self.driver.quit()
 
     def search_product_on_site(self, product, site_domain="https://www.ikesaki.com.br/"):
-        """
-        Objective:
-        Search the domain website for the specific product
-        through automation, and click on the first corresponding product.
-
-        Parameters:
-        site_domain = "https://www.ikesaki.com.br/"
-        product = "Esmalte Vermelho"
-        """
         self.start_driver()
 
         self.driver.get(site_domain)
@@ -37,30 +32,60 @@ class AutomationSearchProduct:
         search_box.send_keys(product, Keys.ENTER)
         time.sleep(5)
 
-        html_content = self.driver.page_source
+        urls = []
 
-        soup = BeautifulSoup(html_content, 'html.parser')
+        while True:
+            html_content = self.driver.page_source
+            soup = BeautifulSoup(html_content, 'html.parser')
+            gallery_div = soup.find('div', {'id': 'gallery-layout-container'})
 
-        gallery_div = soup.find('div', {'id': 'gallery-layout-container'})
-        if gallery_div:
-            first_img = gallery_div.find('img')
-            if first_img:
-                img_element = self.driver.find_element('xpath',
-                                                       '//*[@id="gallery-layout-container"]/div/section/a/article'
-                                                       '/div[1]/div[1]/div/div/img')
+            if gallery_div:
+                img_elements = gallery_div.find_all('img')
+                for i, img in enumerate(img_elements, start=1):
+                    img_xpath = f'//*[@id="gallery-layout-container"]/div[{i}]/section/a/article/div[1]/div[1]/div/div/img'
 
-                img_element.click()
+                    # Aguarda até que o elemento esteja visível
+                    WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.XPATH, img_xpath)))
 
-                time.sleep(3)
+                    img_element = self.driver.find_element('xpath', img_xpath)
 
-                if self.driver.current_url != site_domain:
-                    print("Redirecionado para:", self.driver.current_url)
-                else:
-                    print("Não houve redirecionamento após clicar na imagem.")
+                    try:
+                        # Execute um script JavaScript para clicar no elemento
+                        self.driver.execute_script("arguments[0].click();", img_element)
+                    except Exception:
+                        pass
 
-        time.sleep(5)
-        current_url = self.driver.current_url
+                    time.sleep(3)
+
+                    if self.driver.current_url != site_domain:
+                        print("Redirecionado para:", self.driver.current_url)
+                        urls.append(self.driver.current_url)
+                    else:
+                        print("Não houve redirecionamento após clicar na imagem.")
+
+                    # Volta para a página anterior para clicar na próxima imagem
+                    self.driver.back()
+                    time.sleep(2)
+
+                # Verifica se há próximo botão de página e clica nele
+            next_page_button = self.driver.find_element_by_xpath('//*[@aria-label="Next"]')
+            if next_page_button.get_attribute('aria-disabled') == 'true':
+                break
+            else:
+                next_page_button.click()
+                time.sleep(5)
 
         self.stop_driver()
 
-        return current_url
+        return urls
+
+    @staticmethod
+    def get_xpath_of_element(element):
+        """Obtém o XPath de um elemento BeautifulSoup"""
+        path = []
+        parent = element.parent
+        while parent:
+            if parent.name:
+                path.insert(0, parent.name)
+            parent = parent.parent
+        return '/'.join(path)
