efde63ecc76d68710ad71be7855b6c92f621e060
Mel-Rocha <rochamel73@gmail.com>
2024-05-27 22:54:39 -0300

    calling other methods only if the previous steps are validated

--- apps/caminhoes_e_carretas/routes.py
+++ apps/caminhoes_e_carretas/routes.py
@@ -21,29 +21,31 @@ async def backhoe():
         collect_urls = CaminhoesECarretasAutomation()
         collected_urls, metrics, automation_failure_analysis = collect_urls.backhoe_url_all()
 
-        extraction = BackhoeExtract(collected_urls)
-        data, extract_failure_analysis, not_price = extraction.extract()
+        if collected_urls:
+            extraction = BackhoeExtract(collected_urls)
+            data, extract_failure_analysis, not_price = extraction.extract()
 
-        logging.info(f"Métricas de desempenho ao obter URLs: {metrics}")
-        logging.info(f"Falha ao obter URL, dos seguintes anúncios: {automation_failure_analysis}")
-        logging.info(f"Falha na extração: {extract_failure_analysis}")
-        logging.info(f"Quantidade de Anúncios sem Preço: {len(not_price)}  URLs Correspondentes: {not_price}")
+            logging.info(f"Métricas de desempenho ao obter URLs: {metrics}")
+            logging.info(f"Falha ao obter URL, dos seguintes anúncios: {automation_failure_analysis}")
+            logging.info(f"Falha na extração: {extract_failure_analysis}")
+            logging.info(f"Quantidade de Anúncios sem Preço: {len(not_price)}  URLs Correspondentes: {not_price}")
 
-        new_records_count = await DatabaseManager.save_data_and_get_new_record_count(data)
-        records_with_null_model_code = await DatabaseManager.count_records_with_null_model_code()
+            if data:
+                new_records_count = await DatabaseManager.save_data_and_get_new_record_count(data)
+                records_with_null_model_code = await DatabaseManager.count_records_with_null_model_code()
 
-        if new_records_count > 0:
-            return JSONResponse(status_code=201, content={
-                "message": "Dados cadastrados com sucesso",
-                "Quantidade de novos registros": new_records_count,
-                "Quantidade total de registros sem correlação com Cód. Modelo": records_with_null_model_code
-            })
-        else:
-            return JSONResponse(status_code=200, content={
-                "message": "Nenhum novo dado foi cadastrado",
-                "Quantidade de novos registros": new_records_count,
-                "Quantidade total de registros sem correlação com Cód. Modelo": records_with_null_model_code
-            })
+                if new_records_count > 0:
+                    return JSONResponse(status_code=201, content={
+                        "message": "Dados cadastrados com sucesso",
+                        "Quantidade de novos registros": new_records_count,
+                        "Quantidade total de registros sem correlação com Cód. Modelo": records_with_null_model_code
+                    })
+                else:
+                    return JSONResponse(status_code=200, content={
+                        "message": "Nenhum novo dado foi cadastrado",
+                        "Quantidade de novos registros": new_records_count,
+                        "Quantidade total de registros sem correlação com Cód. Modelo": records_with_null_model_code
+                    })
     except Exception as e:
         logging.error(f"Erro: {str(e)}")
         raise HTTPException(status_code=500, detail="Ocorreu um erro, falha no processo.")
